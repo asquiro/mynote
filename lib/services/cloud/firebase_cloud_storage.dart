9c4ab1c29a6e362162a163a14204e17b1c21a21d
Matyxl <matthewasuquo123@gmail.com>
2023-06-12 00:35:28 +0000

    CRUD functionality in cloud storage

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -4,8 +4,47 @@ import 'package:mypersonalnote/services/cloud/cloud_storage_constants.dart';
 import 'package:mypersonalnote/services/cloud/cloud_storage_exceptions.dart';
 
 class FirebaseCloudStorage {
-  final notes = FirebaseFirestore.instance.collection('notes');
+  final notes = FirebaseFirestore.instance.collection(
+    'notes',
+  );
 
+// deleting note from cloudstore can be expressed as
+
+  Future<void> deleteNotes({required String documentId}) async {
+    try {
+      await notes.doc(documentId).delete();
+    } catch (_) {
+      throw CouldNotCreateNoteException();
+    }
+  }
+
+// using cloudfirestorage to continuously synchronize our note changes to ur firebase
+  Future<void> updateNotes({
+    required String documentId,
+    required String text,
+  }) async {
+    try {
+      await notes.doc(documentId).update(
+        {textFieldName: text},
+      );
+    } catch (_) {
+      throw CouldNotUpdateNoteException();
+    }
+  }
+
+  // a stream to grap all the changes made in the notes and syncronized directly to cloud
+  Stream<Iterable<CloudNote>> allNotes({required String ownersUserId}) =>
+      notes.snapshots().map(
+            (event) => event.docs
+                .map(
+                  (doc) => CloudNote.fromSnapshot(doc),
+                )
+                .where(
+                  (note) => note.ownerUserId == ownersUserId,
+                ),
+          );
+
+  // a function to get all the notes and syncronized it to the cloudfirestore thats the database
   Future<Iterable<CloudNote>> getNotes({
     required String ownerUserID,
   }) async {
